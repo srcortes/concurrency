c4cfcf6fb37f5e357c51a50f04982d60ba4dea93
john.rodriguez <johnrodriguez.globant@latam.com>
2025-08-08 15:59:42 -0500

    To initialize spring boot

---
+++ src/main/java/org/example/controller/ConcurencyTest.java
@@ -0,0 +1,12 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConcurencyTest {
+     @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
